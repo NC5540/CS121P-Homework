b645a2266ba4e45910b04109a619430bcb979adf
NC <nchou00@gmail.com>
2025-09-02 19:56:29 -0700

    Homework02 completed

---
+++ Homework02.py
@@ -0,0 +1,10 @@
+str_kg = input("Please enter the amount of kilograms you wish to convert: ")
+total_kg = float(str_kg)
+
+ounces = total_kg * 35.2739619495804106 
+
+total_pound = int(ounces) // 16
+
+total_ounce = ounces % 16
+
+print(total_kg, "kilograms is", total_pound, "pounds and", total_ounce, "ounces")
