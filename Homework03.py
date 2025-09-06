d4381a354fb261efb49b053769d64bdac6b78708
NC <nchou00@gmail.com>
2025-09-05 22:20:20 -0700

    added color shift to turtle drawing in Homework03

--- Homework03.py
+++ Homework03.py
@@ -9,7 +9,11 @@ spikemeyer = turtle.Turtle()
 for i in range(int(range_num)):
     spikemeyer.forward(i)
     spikemeyer.left(int(angle))
-     
+
+    color_range = (int(range_num) - i) / int(range_num)
+
+    spikemeyer.color(color_range, color_range, color_range)
+
 print("Click turtle screen to exit...")
 
 wn.exitonclick()
