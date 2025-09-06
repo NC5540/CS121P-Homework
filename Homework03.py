9a87b0dee54198519094b6592b7a183cd4b9a5b4
NC <nchou00@gmail.com>
2025-09-05 22:56:29 -0700

    added comments and converted one var to a int

--- Homework03.py
+++ Homework03.py
@@ -1,16 +1,20 @@
 import turtle
 
+# Getting user input
 angle = input("Enter int angle: ")
-range_num = input("Enter int range: ")
+num_range = int(input("Enter int range: "))
 
+# Setting turtle objects
 wn = turtle.Screen()
 spikemeyer = turtle.Turtle()  
 
-for i in range(int(range_num)):
+for i in range(num_range):
+    # Drawing shape with range and angle given
     spikemeyer.forward(i)
     spikemeyer.left(int(angle))
 
-    color_range = (int(range_num) - i) / int(range_num)
+    # Adjusting color with range as drawing progresses
+    color_range = (num_range - i) / num_range
 
     spikemeyer.color(color_range, color_range, color_range)
 
